db258db1237ea9cecbe5fc096d9efbf82b283eb5
Dave Longley <dlongley@digitalbazaar.com>
2020-12-01 14:24:47 -0500

    Remove use of callbacks.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -1,66 +1,78 @@
 /*
- * Copyright (c) 2017-2019 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2017-2020 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
-const async = require('async');
 const bedrock = require('bedrock');
 const brLedgerNode = require('bedrock-ledger-node');
 const database = require('bedrock-mongodb');
+const {promisify} = require('util');
 const uuid = require('uuid/v4');
 
 const api = {};
 module.exports = api;
 
-api.addOperation = ({count = 1, ledgerNode, opTemplate}, callback) => {
+api.addOperation = async ({count = 1, ledgerNode, opTemplate}) => {
   const operations = {};
-  async.timesSeries(count, (i, callback) => {
+  for(let i = 0; i < count; ++i) {
     const operation = bedrock.util.clone(opTemplate);
     // this creator is a required part of the operation
     operation.creator = ledgerNode._peerId;
     operation.record.id = `https://example.com/event/${uuid()}`;
     // this creator is just an arbitrary field in the record
     operation.record.creator = uuid();
-    ledgerNode.operations.add({operation}, (err, result) => {
-      if(err) {
-        return callback(err);
-      }
-      operations[result.meta.operationHash] = operation;
-      callback();
-    });
-  }, err => callback(err, operations));
+    const result = await ledgerNode.operations.add({operation});
+    operations[result.meta.operationHash] = operation;
+  }
+  return operations;
 };
 
-api.prepareDatabase = function(mockData, callback) {
-  async.series([
-    callback => {
-      api.removeCollections([
-        'identity', 'eventLog', 'ledger', 'ledgerNode', 'continuity2017_key',
-        'continuity2017_manifest', 'continuity2017_vote', 'continuity2017_voter'
-      ], callback);
-    },
-  ], callback);
+api.prepareDatabase = async function() {
+  await api.removeCollections([
+    'identity', 'eventLog', 'ledger', 'ledgerNode', 'continuity2017_key',
+    'continuity2017_manifest', 'continuity2017_vote', 'continuity2017_voter'
+  ]);
 };
 
-api.removeCollections = function(collections, callback) {
+api.removeCollections = async function(collections) {
   const collectionNames = [].concat(collections);
-  database.openCollections(collectionNames, () => {
-    async.each(collectionNames, function(collectionName, callback) {
-      if(!database.collections[collectionName]) {
-        return callback();
-      }
-      database.collections[collectionName].deleteMany({}, callback);
-    }, function(err) {
-      callback(err);
-    });
-  });
+  await promisify(database.openCollections)(collectionNames);
+  for(const collectionName of collectionNames) {
+    await database.collections[collectionName].deleteMany({});
+  }
 };
 
-api.runWorkerCycle = ({consensusApi, nodes, series = false}, callback) => {
-  const func = series ? async.eachSeries : async.each;
-  func(nodes, (ledgerNode, callback) =>
-    consensusApi._worker._run(ledgerNode, callback), callback);
+api.runWorkerCycle = async (
+  {consensusApi, nodes, series = false, targetCyclesPerNode = 1}) => {
+  const promises = [];
+  for(const ledgerNode of nodes) {
+    const promise = _cycleNode(
+      {consensusApi, ledgerNode, targetCycles: targetCyclesPerNode});
+    if(series) {
+      await promise;
+    } else {
+      promises.push(promise);
+    }
+  }
+  await Promise.all(promises);
 };
+
+async function _cycleNode({consensusApi, ledgerNode, targetCycles = 1} = {}) {
+  if(ledgerNode.stop) {
+    return;
+  }
+
+  try {
+    await consensusApi._worker._run({ledgerNode, targetCycles});
+  } catch(err) {
+    // if a config change is detected, do not run worker on that node again
+    if(err && err.name === 'LedgerConfigurationChangeError') {
+      ledgerNode.stop = true;
+      return;
+    }
+    throw err;
+  }
+}
 
 /*
  * execute the worker cycle until there are no non-consensus
@@ -69,66 +81,45 @@ api.runWorkerCycle = ({consensusApi, nodes, series = false}, callback) => {
  * there will be various numbers of non-consensus events of type
  * `ContinuityMergeEvent` on a settled network.
  */
-api.settleNetwork = ({consensusApi, nodes, series = false}, callback) => {
-  async.doWhilst(callback => {
-    async.auto({
-      workCycle: callback => api.runWorkerCycle(
-        {consensusApi, nodes, series}, err => {
-          if(err && err.name !== 'LedgerConfigurationChangeError') {
-            return callback(err);
-          }
-          callback();
-        }),
-      operationEvents: ['workCycle', (results, callback) => {
-        async.map(nodes, (ledgerNode, callback) => {
-          ledgerNode.storage.events.getCount({
-            consensus: false, type: 'WebLedgerOperationEvent'
-          }, callback);
-        }, (err, result) => {
-          if(err) {
-            return callback(err);
-          }
-          // all nodes should have zero non-consensus regular events
-          callback(null, result.every(c => c === 0));
-        });
-      }],
-      configEvents: ['operationEvents', (results, callback) => {
-        async.map(nodes, (ledgerNode, callback) => {
-          ledgerNode.storage.events.getCount({
-            consensus: false, type: 'WebLedgerConfigurationEvent'
-          }, callback);
-        }, (err, result) => {
-          if(err) {
-            return callback(err);
-          }
-          // all nodes should have zero non-consensus configuration events
-          callback(null, result.every(c => c === 0));
-        });
-      }],
-      blocks: ['configEvents', (results, callback) => {
-        async.map(nodes, (ledgerNode, callback) => {
-          ledgerNode.storage.blocks.getLatestSummary(callback);
-        }, (err, result) => {
-          if(err) {
-            return callback(err);
-          }
-          const blockHeights = result.map(s => s.eventBlock.block.blockHeight);
-          // all nodes should have the same latest blockHeight
-          callback(null, blockHeights.every(b => b === blockHeights[0]));
-        });
-      }]
-    }, callback);
-  }, results => {
-    return !(results.operationEvents && results.configEvents && results.blocks);
-  }, callback);
+api.settleNetwork = async ({consensusApi, nodes, series = false} = {}) => {
+  while(true) {
+    await api.runWorkerCycle({consensusApi, nodes, series});
+
+    // all nodes should have zero non-consensus regular events
+    let promises = [];
+    for(const ledgerNode of nodes) {
+      promises.push(ledgerNode.storage.events.getCount({
+        consensus: false, type: 'WebLedgerOperationEvent'
+      }));
+    }
+    if((await Promise.all(promises)).some(c => c > 0)) {
+      continue;
+    }
+
+    // all nodes should have zero non-consensus configuration events
+    promises = [];
+    for(const ledgerNode of nodes) {
+      promises.push(ledgerNode.storage.events.getCount({
+        consensus: false, type: 'WebLedgerConfigurationEvent'
+      }));
+    }
+    if((await Promise.all(promises)).some(c => c > 0)) {
+      continue;
+    }
+
+    // all nodes should have the same latest blockHeight
+    promises = [];
+    for(const ledgerNode of nodes) {
+      promises.push(ledgerNode.storage.blocks.getLatestSummary());
+    }
+    const summaries = await Promise.all(promises);
+    const blockHeights = summaries.map(s => s.eventBlock.block.blockHeight);
+    if(blockHeights.every(b => b === blockHeights[0])) {
+      break;
+    }
+  }
 };
 
-api.use = (plugin, callback) => {
-  let p;
-  try {
-    p = brLedgerNode.use(plugin);
-  } catch(e) {
-    return callback(e);
-  }
-  callback(null, p);
+api.use = async plugin => {
+  return brLedgerNode.use(plugin);
 };
